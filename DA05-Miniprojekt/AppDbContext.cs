57aa87d0e6c4225fbfb980a166f2548927cc0e51
Alexander Berg <72527002+johnalexanderberg@users.noreply.github.com>
2021-11-03 16:48:51 +0100

    read key

--- DA05-Miniprojekt/AppDbContext.cs
+++ DA05-Miniprojekt/AppDbContext.cs
@@ -12,9 +12,9 @@ namespace DA05_Miniprojekt
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
 
-            string key = File.ReadAllText("Key.txt");   
+            var key = File.ReadAllText("Key.txt");   
 
-            options.UseSqlServer(@"Data Source=den1.mssql7.gear.host;Initial Catalog=consoleapp;User Id=consoleapp; Password=; Trusted_Connection=false; MultipleActiveResultSets=true;");
+            options.UseSqlServer(@"Data Source=den1.mssql7.gear.host;Initial Catalog=consoleapp;User Id=consoleapp; Password={key}; Trusted_Connection=false; MultipleActiveResultSets=true;");
         }
     }
 
